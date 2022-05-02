589587e9b4616d9b6142e1187229703d80d967a5
Supanat Wangsutthitham <supa7412@gmail.com>
2022-05-02 18:38:26 -0400

    upload omz installing shells

---
+++ omz-install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Installing OMZ
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Installing Plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Installing P10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
