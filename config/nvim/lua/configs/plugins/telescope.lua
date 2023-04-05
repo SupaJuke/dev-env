7b149ce47562289e7a96fd0efe7100162aec2e18
Supanat Wangsutthitham <soup.wangsutthitham@mail.utoronto.ca>
2023-04-04 23:41:53 -0400

    feat: add font & fix telescope extensions not working

--- config/nvim/lua/configs/plugins/telescope.lua
+++ config/nvim/lua/configs/plugins/telescope.lua
@@ -9,20 +9,17 @@ function M.setup()
 
   -- Setup
   telescope.setup {
-    file_browser = {
-      -- theme = "ivy",
-      -- disables netrw and use telescope-file-browser in its place
-      hijack_netrw = true,
-      mappings = {
-        ["i"] = {
-          -- your custom insert mode mappings
-        },
-        ["n"] = {
-          -- your custom normal mode mappings
-        },
+    extensions = {
+      fzf = {
+        fuzzy = true,
+        override_generic_sorter = true,
+        override_file_sorter = true,
+        case_mode = "smart_case",
       },
-    },
-    extensions = {}
+      file_browser = {
+        hijack_netrw = true,
+      }
+    }
   }
 
   telescope.load_extension('fzf')
