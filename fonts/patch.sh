97326ca757a37c7c178563e1df1ec312197d1b06
Supanat Wangsutthitham <soup.wangsutthitham@mail.utoronto.ca>
2023-04-10 17:38:16 -0400

    chore: removed unused fonts

--- fonts/patch.sh
+++ fonts/patch.sh
@@ -6,9 +6,9 @@ then
     exit
 fi
 
-FONT_DIR=$1
+FONT_DIR=$(dirname $1)
 FLAGS="--complete"
-echo $FONT
+
 if [[ $# -gt 1 ]]
 then
     FLAGS="$FLAGS ${@:2}"
@@ -18,5 +18,5 @@ for FILE in $FONT_DIR/*.*tf
 do
     # echo "ran: fontforge -script FontPatcher/font-patcher $FLAGS -out "${FONT}_patched" $FILE" # For debug purposes
     # echo $FILE
-    fontforge -script FontPatcher/font-patcher $FLAGS -out "$(echo $FONT_DIR | cut -d '/' -f 1)/patched/" $FILE
+    fontforge -script FontPatcher/font-patcher $FLAGS -out ${FONT_DIR}/patched/ $FILE
 done
