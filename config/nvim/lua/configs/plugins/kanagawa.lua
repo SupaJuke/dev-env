08bb728578ea701a4ecfc6117983b38a0d03be22
Supanat Wangsutthitham <supa7412@gmail.com>
2025-04-24 01:53:19 +0200

    feat: update kanagawa theme (nvim and windows_terminal)

--- config/nvim/lua/configs/plugins/kanagawa.lua
+++ config/nvim/lua/configs/plugins/kanagawa.lua
@@ -1,6 +1,43 @@
 local M = {}
+local kanagawa = require("kanagawa")
 
 function M.setup()
+  kanagawa.setup({
+    compile = false,  -- enable compiling the colorscheme
+    undercurl = true, -- enable undercurls
+    commentStyle = { italic = true },
+    functionStyle = {},
+    keywordStyle = { italic = true },
+    statementStyle = { bold = true },
+    typeStyle = {},
+    transparent = false,   -- do not set background color
+    dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
+    terminalColors = true, -- define vim.g.terminal_color_{0,17}
+    colors = {             -- add/modify theme and palette colors
+      palette = {},
+      theme = {
+        wave = {},
+        lotus = {},
+        dragon = {},
+        all = {
+          ui = {
+            -- default doesn't exist
+            bg_gutter = "none"
+          }
+        }
+      },
+    },
+
+    overrides = function(colors) -- add/modify highlights
+      return {}
+    end,
+
+    theme = "dragon",  -- Load "wave" theme
+    background = {     -- map the value of 'background' option to a theme
+      dark = "dragon", -- try "dragon" !
+      light = "lotus"
+    },
+  })
 end
 
 return M
