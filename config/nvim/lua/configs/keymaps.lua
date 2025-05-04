5a7744ee160de0ccfba67ca378c8d5694914689a
Supanat Wangsutthitham <supa7412@gmail.com>
2025-05-04 20:43:54 +0200

    feat: add keymap to undo highlight and exit terminal mode
    Also update Lazy plugins

--- config/nvim/lua/configs/keymaps.lua
+++ config/nvim/lua/configs/keymaps.lua
@@ -3,6 +3,8 @@ local keymap = vim.keymap.set
 
 function M.setup()
   -- keymap('n', '<Home>', "0")
+  keymap('t', '<Esc>', [[<C-\><C-n>]], { noremap = true })
+  keymap('n', '<Esc>', ":noh<CR>", { noremap = true })
 end
 
 return M
