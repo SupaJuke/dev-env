9b8d6dce03c19a5c76b2cc61c1d077e053fd8187
Supanat Wangsutthitham <supa7412@gmail.com>
2026-04-21 00:57:30 +0200

    feat: update nvim-autopairs (esp for latex)

--- config/nvim/lua/configs/plugins/autopairs.lua
+++ config/nvim/lua/configs/plugins/autopairs.lua
@@ -4,18 +4,22 @@ local Rule = require("nvim-autopairs.rule")
 local cond = require("nvim-autopairs.conds")
 
 function M.setup()
-  npairs.setup({})
+  npairs.setup()
 
-  -- Disable the built-in `[` rule for TeX-ish filetypes
-  for _, rule in ipairs(npairs.get_rules("[")) do
-    rule.not_filetypes = { "tex", "plaintex", "latex" }
-  end
+  -- Latex stuffs
+  npairs.add_rules({
+    Rule("$", "$", { "tex", "latex" })
+        :with_pair(cond.not_before_text("\\"))
+        :with_pair(function(opts)
+          return opts.next_char ~= "$"
+        end)
+        :with_move(function(opts)
+          return opts.next_char == "$"
+        end),
 
-  -- Re-add a TeX-only `[` rule, but don't pair after a backslash
-  npairs.add_rule(
-    Rule("[", "]", { "tex", "plaintex", "latex" })
-    :with_pair(cond.not_before_regex("\\\\", 1))
-  )
+    Rule("\\(", "\\)", { "tex", "latex" }),
+    Rule("\\[", "\\]", { "tex", "latex" })
+  })
 end
 
 return M
